ec243bd8bad8dd91b7227c7b2306464f0242a474
Piotr-Karitas <azraeldark@02.pl>
2024-06-26 19:13:36 +0200

    style ktore niestety dzialaja w teorii tylko i nie wiem czemu nie w praktyce :(((

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">refresh</button>
-        siema blog!
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+    <h2 class="text-blue-600">Wpisy na bloga</h2>
+    <div class="w-100 flex flex-row-reverse">
+        <button @click="pobierzWpisy" class="bg-blue-600 rounded-md text-white p-4">refresh</button>
     </div>
+    <div class="grid mx-6 gap-4 my-4">
+        <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-300 p-4" >
+        <p>  {{ wpis }}  </p>
+        </div>
+        </div>
+
+                <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4">
+            <button @click="dodajWpisy" class="bg-blue-600 rounded-md text-white p-4">dodaj</button>
+                </div>
+     </div>
 </template>
 
 <script>
@@ -26,6 +35,8 @@ export default {
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
     },
-
+ async mounted(){
+        this.pobierzWpisy()
+    },
 }
 </script>
