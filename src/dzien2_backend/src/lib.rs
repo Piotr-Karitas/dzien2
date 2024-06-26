0c6625a6125a16d3a1096e5614618aa11b78000a
Piotr-Karitas <azraeldark@02.pl>
2024-06-26 17:37:28 +0200

    added usuwanie i edytowanie wpisow

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -8,7 +8,7 @@ thread_local! {
 fn greet(name: String, last_name: i8) -> String {
     format!("Hello, {} {}!", name, last_name)
 }
-
+ 
 #[ic_cdk::update]
 fn dodaj_wpis(wpis: String) {
     WPISY.with(|wpisy| {
@@ -21,4 +21,21 @@ fn odczytaj_wpisy() -> Vec<String> {
     WPISY.with(|wpisy| {
         wpisy.borrow().clone()
     })
+}
+
+#[ic_cdk::update]
+fn usun_wpis(id_wpisu: usize){
+    WPISY.with(|wpisy:&RefCell<Vec<String>>|{
+    wpisy.borrow_mut().remove(id_wpisu)
+ });
+}
+
+#[ic_cdk::update]
+fn edytuj_wpis(id_wpisu: usize, nowy_wpis: String){
+    WPISY.with(|wpisy:&RefCell<Vec<String>>|{
+        let mut binding= wpisy.borrow_mut();
+        let wpis = binding.get_mut(id_wpisu);
+        let stary_wpis = wpis.unwrap(); 
+        *stary_wpis = nowy_wpis;
+    });
 }
